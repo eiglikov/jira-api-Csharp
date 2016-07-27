f7ca4b54f40afbb784cd6bafaa8d633a037e0754
Erik Iglikov <sneanone@gmail.com>
2016-07-27 18:15:06 -0400

    initial commit

---
+++ JIRA test/JiraManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+public enum JiraResource
+{
+    project
+}
+
+public class JiraManager
+{
+    private const string m_BaseUrl = "https://wearejam3.atlassian.net/rest/api/latest/issue/JAMHOLO-14";
+    //JAMHOLO-14
+    private string m_Username;
+    private string m_Password;
+
+    public JiraManager(string username, string password)
+    {
+        m_Username = username;
+        m_Password = password;
+    }
+
+    public void RunQuery(
+        JiraResource resource,
+        string argument = null,
+        string data = null,
+        string method = "GET")
+    {
+        //string url = string.Format("{0}{1}/", m_BaseUrl, resource.ToString());
+        string url = m_BaseUrl;
+        if (argument != null)
+        {
+            url = string.Format("{0}{1}/", url, argument);
+        }
+
+        HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+        request.ContentType = "application/json";
+        request.Method = method;
+
+        if (data != null)
+        {
+            using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
+            {
+                writer.Write(data);
+            }
+        }
+
+        string base64Credentials = GetEncodedCredentials();
+        request.Headers.Add("Authorization", "Basic " + base64Credentials);
+
+        HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+        string result = string.Empty;
+        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+        {
+            result = reader.ReadToEnd();
+        }
+
+        Console.WriteLine(result);
+    }
+
+    private string GetEncodedCredentials()
+    {
+        string mergedCredentials = string.Format("{0}:{1}", m_Username, m_Password);
+        byte[] byteCredentials = UTF8Encoding.UTF8.GetBytes(mergedCredentials);
+        return Convert.ToBase64String(byteCredentials);
+    }
+}
---
+++ JIRA test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JIRA_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to a Jira Example application!");
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            //string username = "";
+            //string password = "";
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            JiraManager manager = new JiraManager(username, password);
+            manager.RunQuery(JiraResource.project);
+
+            Console.Read();
+        }
+    }
+}
